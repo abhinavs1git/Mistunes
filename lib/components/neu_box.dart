e5187b8e6f4bb454917e3bc96929fbca46d9e6f9
abhinavs1git <150845670+abhinavs1git@users.noreply.github.com>
2024-02-18 13:57:11 +0530

    Update neu_box.dart

--- lib/components/neu_box.dart
+++ lib/components/neu_box.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:mistunes/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class NeuBox extends StatelessWidget {
   final Widget? child;
-  const NeuBox({super.key,
-  required this.child,});
+  const NeuBox({
+    super.key,
+    required this.child,
+  });
 
   @override
   Widget build(BuildContext context) {
+    bool isDarkMode = Provider.of<ThemeProvider>(context).isDarkMode;
     return Container(
       decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.background,
-          borderRadius: BorderRadius.circular(12),
+          borderRadius: BorderRadius.circular(24),
           boxShadow: [
             BoxShadow(
-                color: Colors.grey.shade500,
+                color: isDarkMode ? Colors.black : Colors.grey.shade500,
                 blurRadius: 15,
                 offset: const Offset(4, 4)),
-            const BoxShadow(
-                color: Colors.white, blurRadius: 15, offset: Offset(-4, -4)),
+            BoxShadow(
+                color: isDarkMode ? Colors.grey.shade800 : Colors.white,
+                blurRadius: 15,
+                offset: const Offset(-4, -4)),
           ]),
-          padding:const EdgeInsets.all(12),
-          child: child,
+      padding: const EdgeInsets.all(12),
+      child: child,
     );
   }
 }
